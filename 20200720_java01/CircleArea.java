2e275aa26377d82b9c90851a6b2e37b4c7f1b873
송원준 <dnjswnsthd@naver.com>
2020-07-22 00:36:43 +0000

    자바 1일차 기본 개념 과제 제출

---
+++ 20200720_java01/CircleArea.java
@@ -0,0 +1,8 @@
+package com.java.first;
+
+public class CircleArea {
+	public static void main(String[] args) {
+		int r = 5;
+		System.out.printf("반지름이 5Cm인 원의 넓이는 : %f cm2입니다.", r*r*Math.PI);
+	}
+}
