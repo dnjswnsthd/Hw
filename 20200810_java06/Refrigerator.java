485eb49cc869970fc783108a4f5d395bea3b1441
송원준 <dnjswnsthd@naver.com>
2020-08-10 11:22:16 +0000

    Refrigerator.java

---
+++ 20200810_java06/Refrigerator.java
@@ -0,0 +1,27 @@
+package com.ssafy.java.day06.hw;
+
+class Refrigerator extends Product {
+	private int size;
+	
+	public Refrigerator(String serial, String name, int price, int count,
+			int size) {
+		super(serial, name, price, count);
+		// TODO Auto-generated constructor stub
+		this.size=size;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString() + String.format(" | %5dL",
+				this.getSize());
+	}	
+}
