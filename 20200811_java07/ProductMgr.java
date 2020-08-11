92aff47cd56d75d8d41d25e1e6393057960eded4
송원준 <dnjswnsthd@naver.com>
2020-08-11 09:39:46 +0000

    자바 7일차 과제 제품관리 프로젝트

---
+++ 20200811_java07/ProductMgr.java
@@ -0,0 +1,17 @@
+package com.ssafy.java.day07.hw;
+import java.util.ArrayList;
+
+interface ProductMgr {
+	public ArrayList<Product> getAl();
+	public void setAl(ArrayList<Product> al);
+	public void printAll();
+	public void findName(String s) ;
+	public void findSerial(String s);
+	public void findTV();
+	public void findRefrigerator();	
+	public void fingBigRefrigerator();
+	public void findBigTV();
+	public void modifyPrice(String s, int price) ;
+	public void deleteWithSerial(String s);
+	public long calcAllPrice() ;
+}
