f6b891168c5e34df8100fe4058ebffdaf4f04b51
송원준 <dnjswnsthd@naver.com>
2020-08-12 10:35:31 +0000

    TV.java

---
+++ 20200812_java08/TV.java
@@ -0,0 +1,32 @@
+package com.ssafy.java.day07.hw;
+
+class TV extends Product {
+	private int inch;
+	private String displayType;
+	
+	public TV(String serial, String name, int price, int count,
+			int inch, String displayType) {
+		super(serial, name, price, count);
+		// TODO Auto-generated constructor stub
+		this.inch = inch;
+		this.displayType=displayType;
+	}
+	public int getInch() {
+		return inch;
+	}
+	public void setInch(int inch) {
+		this.inch = inch;
+	}
+	public String getDisplayType() {
+		return displayType;
+	}
+	public void setDisplayType(String displayType) {
+		this.displayType = displayType;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString() + String.format(" | %-5dInch | %-8s",
+				this.getInch(), this.getDisplayType());
+	}
+}
