b826d8869e2a86c978b323bcfb00bb772e397eae
송원준 <dnjswnsthd@naver.com>
2020-08-12 10:34:22 +0000

    ProductNotFoundException.java

---
+++ 20200812_java08/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ssafy.java.day07.hw;
+
+public class ProductNotFoundException extends Exception{
+	private static final long serialVersionUID = 1L;
+	public ProductNotFoundException(String msg) {
+		super(msg);
+	}
+}
