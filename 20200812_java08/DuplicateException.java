a267027606a8d27645516d4221c948c2521ecc09
송원준 <dnjswnsthd@naver.com>
2020-08-12 10:33:32 +0000

    DuplicateException.java

---
+++ 20200812_java08/DuplicateException.java
@@ -0,0 +1,8 @@
+package com.ssafy.java.day07.hw;
+
+public class DuplicateException extends Exception{
+	private static final long serialVersionUID = 1L;
+	public DuplicateException(String msg) {
+		super(msg);
+	}
+}
