ff81c20955621009f024d5f4a0167a50055d5dcc
송원준 <dnjswnsthd@naver.com>
2020-08-19 06:57:15 +0000

    자바 11일차 과제

---
+++ 20200818_java11/QuantityException.java
@@ -0,0 +1,8 @@
+package com.ssafy.java.day11.hw;
+
+public class QuantityException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public QuantityException(String msg) {
+		super(msg);
+	}
+}
