12a13eb1fc04f379b87ba1c01403cffca8a18347
송원준 <dnjswnsthd@naver.com>
2020-08-19 06:57:29 +0000

    자바 11일차 과제

---
+++ 20200818_java11/SerialNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ssafy.java.day11.hw;
+
+public class SerialNotFoundException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public SerialNotFoundException(String msg) {
+		super(msg);
+	}
+}
